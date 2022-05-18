5d58a766419f61cf55672cb746d8a851fd9c1d9f
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2022-05-18 13:52:20 +0200

    Completed Home Page and Navigation

--- js/jss.js
+++ js/jss.js
@@ -1,17 +1,4 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
-
-function hideLoader() {
-    $('#loader').hide();
-}
-$(window).ready(hideLoader);
-setTimeout(hideLoader, 20 * 1000);
+function menuToggle(){
+  const toggleMenu = document.querySelector(".toggle");
+  toggleMenu.classList.toggle("active");
+};
