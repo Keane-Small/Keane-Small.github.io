9adac71d5f8ea5930debfadc036b4bf529a772bb
Keane Small <keanesmall99@gmail.com>
2025-03-18 23:51:31 +0200

    newAngularWebsite

---
+++ src/app/current-project/current-project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-project',
+  imports: [],
+  templateUrl: './current-project.component.html',
+  styleUrl: './current-project.component.css'
+})
+export class CurrentProjectComponent {
+
+}
---
+++ src/app/currently-learning/currently-learning.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currently-learning',
+  imports: [],
+  templateUrl: './currently-learning.component.html',
+  styleUrl: './currently-learning.component.css'
+})
+export class CurrentlyLearningComponent {
+
+}
