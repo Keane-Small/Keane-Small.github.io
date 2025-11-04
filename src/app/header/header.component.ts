7c47c71a342f9658666a824ac8c0046797cd8ff5
Keane Small <48247528+Keane-Small@users.noreply.github.com>
2025-11-04 11:21:39 +0200

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -9,26 +10,41 @@ import { Component, HostListener } from '@angular/core';
 export class HeaderComponent {
   activeSection = '';
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   setActiveSection(section: string) {
     this.activeSection = section;
-}
+  }
 
-@HostListener('window:scroll', ['$event'])
-onWindowScroll() {
-  const sections = document.querySelectorAll('section');
-  let foundSection = '';
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    if (!isPlatformBrowser(this.platformId)) return;
 
-  sections.forEach((section:any ) => {
-    const rect = section.getBoundingClientRect();
-    if (rect.top >= 0 && rect.top <= window.innerHeight / 2){
-      foundSection = section.id;
+    // Update scroll progress bar
+    const scrollProgress = document.querySelector('.scroll-progress') as HTMLElement;
+    if (scrollProgress) {
+      const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+      const scrolled = (window.scrollY / windowHeight) * 100;
+      scrollProgress.style.width = scrolled + '%';
     }
-  });
 
-  this.activeSection = foundSection;
-}
-ngOnInit() {
-  this.onWindowScroll();
-}
+    // Update active section
+    const sections = document.querySelectorAll('section');
+    let foundSection = '';
 
+    sections.forEach((section: any) => {
+      const rect = section.getBoundingClientRect();
+      if (rect.top >= 0 && rect.top <= window.innerHeight / 2) {
+        foundSection = section.id;
+      }
+    });
+
+    this.activeSection = foundSection;
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.onWindowScroll();
+    }
+  }
 }
